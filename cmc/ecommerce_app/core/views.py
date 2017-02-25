513a39297a48812fe963de58d5170bc10f5aa2d8
seul <ingzarragaespinosa@gmail.com>
2017-02-25 16:40:04 -0600

    csrf

--- cmc/ecommerce_app/core/views.py
+++ cmc/ecommerce_app/core/views.py
@@ -26,7 +26,6 @@ from django.utils.http import urlsafe_base64_decode
 from django.utils.translation import ugettext as _
 from django.views.decorators.cache import never_cache
 from django.views.decorators.csrf import csrf_protect
-from django.views.decorators.csrf import ensure_csrf_cookie
 from django.views.decorators.debug import sensitive_post_parameters
 from django.views.decorators.http import require_POST, require_GET
 from django.conf import settings
@@ -165,7 +164,7 @@ def search(request, **kwargs):
                                          'Page'}, context_instance=RequestContext(request))
 
 
-@ensure_csrf_cookie
+@csrf_protect
 def register(request):
     context = RequestContext(request)
 
@@ -239,7 +238,7 @@ def register(request):
     )
 
 
-@ensure_csrf_cookie
+@csrf_protect
 def user_login(request):
     context = RequestContext(request)
 
