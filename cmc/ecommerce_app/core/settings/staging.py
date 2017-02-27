e316d6993a56728716c1e86f50429b35d521cbea
seul <ingzarragaespinosa@gmail.com>
2017-02-27 10:54:28 -0600

    cookies removed in settings

--- cmc/ecommerce_app/core/settings/staging.py
+++ cmc/ecommerce_app/core/settings/staging.py
@@ -20,17 +20,6 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-CSRF_COOKIE_HTTPONLY = True
-CSRF_COOKIE_SECURE = False
-SESSION_COOKIE_HTTPONLY = True
-SESSION_COOKIE_SECURE = False
-
-SECURE_HSTS_SECONDS = 1
-SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-SECURE_CONTENT_TYPE_NOSNIFF = True
-SECURE_BROWSER_XSS_FILTER = True
-SECURE_SSL_REDIRECT = False
-X_FRAME_OPTIONS = 'DENY'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
@@ -62,4 +51,3 @@ ALGOLIA = {
     'SEARCH_KEY': os.environ.get('SEARCH_KEY'),
     'INDEX_PREFIX': 'staging',
 }
-
