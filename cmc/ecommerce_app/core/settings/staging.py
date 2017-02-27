937e15b3c1d3a9b614237ccf9b332df79214dc6d
seul <ingzarragaespinosa@gmail.com>
2017-02-27 11:35:06 -0600

    reverse cookies settings

--- cmc/ecommerce_app/core/settings/staging.py
+++ cmc/ecommerce_app/core/settings/staging.py
@@ -23,8 +23,16 @@ DEBUG = True
 ALLOWED_HOSTS = ['comercializadora.herokuapp.com']
 
 CSRF_COOKIE_HTTPONLY = True
+CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_HTTPONLY = True
+SESSION_COOKIE_SECURE = True
 
+SECURE_HSTS_SECONDS = 1
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_SSL_REDIRECT = False
+X_FRAME_OPTIONS = 'DENY'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
@@ -56,3 +64,4 @@ ALGOLIA = {
     'SEARCH_KEY': os.environ.get('SEARCH_KEY'),
     'INDEX_PREFIX': 'staging',
 }
+
